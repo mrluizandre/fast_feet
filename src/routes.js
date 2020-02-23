8089cd450175b41188ba65baf1c81c558ba72a59
Andre Costa <mr.luizandre@gmail.com>
2020-02-23 19:25:09 -0300

    Upload and list files

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import RecipientController from './app/controller/RecipientController';
+import FileController from './app/controller/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 import loggerMiddleware from './app/middlewares/logger';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.use(loggerMiddleware);
 
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/users', UserController.store);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
